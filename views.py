7b4cba8d3b01a76d94aff41bf745a57eea1a1b19
Alfonso <alfonsobarrera_1@hotmail.com>
2020-02-22 21:51:59 -0500

    update

--- views.py
+++ views.py
@@ -1,9 +1,34 @@
-from flask import Flask
+
+
+
+from flask import Flask, request, redirect, url_for, jsonify
+import logging
+
 app=Flask(__name__)
+app.debug=True
+
 
 @app.route("/")
 def hello():
-	return "stupid"
+	return "Welcome to credman\n"
+	
+@app.route("/add", methods=['POST'])
+def add_credential():
+	dic={}
 
+	try:
+		form=request.form
+		for i in form:
+			dic[i]=form[i]
+		
+		return jsonify(dic)
+	except Exception as e:
+		logging.warning("Exception was detected".format(e))
+		return ""
+
+		
+	
+	
 if __name__=="__main__":
 	app.run(host='0.0.0.0')
+	
